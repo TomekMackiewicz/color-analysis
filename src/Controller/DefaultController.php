f60bde2d014d98b17bfa9a6faa466db61bc29af7
Tomasz Mackiewicz <tomasz.mackiewicz79@gmail.com>
2018-08-26 22:13:57 +0200

    Testing routing and controller

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class DefaultController extends AbstractController
+{
+    
+    /**
+    * @Route("/")
+    */    
+    public function index()
+    {
+        return $this->render('base.html.twig', [
+
+        ]);
+    }
+}
+
